d5a5be587fdf9fbe8cc3284a81bece4894f610d6
hmcommercial1709-creator <hmcommercial1709@gmail.com>
2026-08-18 23:05:14 +0300

    Update $locale.index.tsx

--- src/routes/$locale.index.tsx
+++ src/routes/$locale.index.tsx
@@ -1,6 +1,10 @@
-import { createFileRoute, Navigate } from '@tanstack/react-router';
+import { createFileRoute, Navigate, notFound } from '@tanstack/react-router';
+import { isLocaleCode } from "@/lib/i18n";
 
 export const Route = createFileRoute('/$locale/')({
+  beforeLoad: ({ params }) => {
+    if (!isLocaleCode(params.locale)) throw notFound();
+  },
   component: () => {
     const { locale } = Route.useParams();
     return <Navigate to={`/${locale}/store`} />;

66289ad39357809a689c0b899e20b5542c898e11
hmcommercial1709-creator <hmcommercial1709@gmail.com>
2026-09-05 09:56:34 +0300

    Enhance sitemap route with XML generation
    Updated the sitemap route to include a loader that generates XML content for multiple sub-sitemaps.

--- src/routes/sitemap.xml.tsx
+++ src/routes/sitemap.xml.tsx
@@ -1,5 +1,36 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute("/sitemap/xml")({
-  beforeLoad: () => { throw redirect({ href: "/sitemap.xml", statusCode: 301 }); },
+export const Route = createFileRoute("/sitemap.xml")({
+  loader: async () => {
+    const baseUrl = "https://gamecastle.store";
+    
+    // قائمة السيتامب الفرعية الموجودة في مشروعك
+    const subSitemaps = [
+      "sitemap-anime.xml",
+      "sitemap-ar.xml",
+      "sitemap-articles.xml",
+      "sitemap-characters.xml",
+      "sitemap-episodes.xml",
+      "sitemap-pages.xml",
+      "sitemap-products.xml",
+      "sitemap-taxonomy.xml"
+    ];
+
+    const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${subSitemaps
+  .map(
+    (sitemap) => `  <sitemap>
+    <loc>${baseUrl}/${sitemap}</loc>
+  </sitemap>`
+  )
+  .join("\n")}
+</sitemapindex>`;
+
+    return new Response(xmlContent, {
+      headers: {
+        "Content-Type": "application/xml; charset=utf-8",
+      },
+    });
+  },
 });

0c8a016700c5ec0ac46d5c5fc5120233d1e205cb
hmcommercial1709-creator <hmcommercial1709@gmail.com>
2026-09-06 07:53:32 +0300

    Refactor sitemap route to use async GET function

--- src/routes/sitemap[.].xml.ts
+++ src/routes/sitemap[.].xml.ts
@@ -1,12 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router';
 import { sitemapIndexXml, xmlResponse } from '@/lib/sitemap';
 
-export const Route = createFileRoute('/sitemap[.]xml')({
-  server: {
-    handlers: {
-      GET: async () => {
-        return xmlResponse(sitemapIndexXml());
-      },
-    },
-  },
-});
+export async function GET() {
+  return xmlResponse(sitemapIndexXml());
+}

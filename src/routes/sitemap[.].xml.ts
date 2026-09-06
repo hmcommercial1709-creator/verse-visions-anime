fdc3be7f4d07ed10e9e5ed3e7c65c35fab044e76
hmcommercial1709-creator <hmcommercial1709@gmail.com>
2026-09-06 08:11:37 +0300

    Delete src/routes/sitemap[.].xml.ts

--- src/routes/sitemap[.].xml.ts
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import type {} from "@tanstack/react-start";
-import { sitemapIndexXml, xmlResponse } from "@/lib/sitemap";
-
-export const Route = createFileRoute("/sitemap.xml")({
-  server: { handlers: { GET: async () => xmlResponse(sitemapIndexXml()) } },
-});

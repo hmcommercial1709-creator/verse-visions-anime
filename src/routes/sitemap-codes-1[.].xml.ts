91bfa2193efa08833c1631d1f5627abcdb2d2533
hmcommercial1709-creator <hmcommercial1709@gmail.com>
2026-09-07 01:48:45 +0300

    Refactor sitemap XML generation for efficiency

--- src/routes/sitemap-codes-1[.].xml.ts
+++ src/routes/sitemap-codes-1[.].xml.ts
@@ -3,18 +3,12 @@ import { xmlResponse, BASE_URL } from '@/lib/sitemap';
 
 export const Route = createFileRoute('/sitemap-codes-1[.]xml')({
   loader: () => {
-    const urls: string[] = [];
+    let urls = '';
     for (let i = 1; i <= 40000; i++) {
-      urls.push(`/gaming-hub/code-${i}`);
+      urls += `  <url><loc>${BASE_URL}/gaming-hub/code-${i}</loc></url>\n`;
     }
 
-    const xml = [
-      `<?xml version="1.0" encoding="UTF-8"?>`,
-      `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`,
-      ...urls.map(path => `  <url><loc>${BASE_URL}${path}</loc></url>`),
-      `</urlset>`
-    ].join('\n');
-
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls}</urlset>`;
     return xmlResponse(xml);
   },
 });

f961c5ffccbb0d46cf36a1f4ac9546bbaf18339d
hmcommercial1709-creator <hmcommercial1709@gmail.com>
2026-09-06 08:15:58 +0300

    Add sitemap.xml route for XML response

---
+++ src/routes/sitemap[.]xml.ts
@@ -0,0 +1,13 @@
+import { createFileRoute } from "@tanstack/react-router";
+import type {} from "@tanstack/react-start";
+import { sitemapIndexXml, xmlResponse } from "@/lib/sitemap";
+
+export const Route = createFileRoute("/sitemap.xml")({
+  server: {
+    handlers: {
+      GET: async () => {
+        return xmlResponse(sitemapIndexXml());
+      },
+    },
+  },
+});

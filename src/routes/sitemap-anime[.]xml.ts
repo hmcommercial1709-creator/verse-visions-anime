7ae59e3175956b47b3fe7638494b23df520ff2c4
hmcommercial1709-creator <hmcommercial1709@gmail.com>
2026-09-11 18:08:50 +0300

    Update sitemap-anime[.]xml.ts

--- src/routes/sitemap-anime[.]xml.ts
+++ src/routes/sitemap-anime[.]xml.ts
@@ -1,7 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
-import { partitionEntries, urlsetXml, xmlResponse } from "@/lib/sitemap";
+import { partitionEntries, urlsetXml } from "@lib/sitemap";
 
 export const Route = createFileRoute("/sitemap-anime.xml")({
-  server: { handlers: { GET: async () => xmlResponse(urlsetXml(partitionEntries("anime"))) } },
+  server: { 
+    handlers: { 
+      GET: async () => {
+        const xmlContent = urlsetXml(partitionEntries("anime"));
+        return new Response(xmlContent, {
+          headers: {
+            "Content-Type": "application/xml; charset=utf-8",
+            "Cache-Control": "public, max-age=3600",
+          },
+        });
+      } 
+    } 
+  },
 });
